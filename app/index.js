d3d77016ddb14c16b7cc1ec0dfb835302933ff16
José Francisco Peña <jose.francisco.pees@gmail.com>
2020-09-08 21:58:42 -0500

    chore: move App component to it's own file

--- app/index.js
+++ app/index.js
@@ -2,21 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "../node_modules/milligram/dist/milligram.min.css";
 
-import SearchBar from "./components/SearchBar";
-
-class App extends React.Component {
-
-  searchUser(user) {
-    console.log('SEARCHING USER: ', user);
-  }
-
-  render() {
-    return (
-      <div className="container">
-        <SearchBar onSearch={this.searchUser} />
-      </div>
-    )
-  }
-}
+import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById('app'));
