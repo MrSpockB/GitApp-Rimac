17da1569bbba42507327190a5128478f03459d8d
José Francisco Peña <jose.francisco.pees@gmail.com>
2020-09-08 21:12:11 -0500

    feat: add SearchBar component file

---
+++ app/components/SearchBar/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SearchBar = ({  }) => (
+  <div>
+    <span>Enter a Github user</span>
+    <input type="text"/>
+    <button>Search</button>
+  </div>
+);
+
+export default SearchBar;
--- app/index.js
+++ app/index.js
@@ -2,15 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "../node_modules/milligram/dist/milligram.min.css";
 
+import SearchBar from "./components/SearchBar";
+
 class App extends React.Component {
   render() {
     return (
       <div className="container">
-        <h1>Hello World</h1>
-        <h2>{this.props.name}</h2>
+        <SearchBar />
       </div>
     )
   }
 }
 
-ReactDOM.render(<App name="Paco" />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
