a5a14001f40ad58f4824b384f7520f836e249812
José Francisco Peña <jose.francisco.pees@gmail.com>
2020-09-08 21:58:18 -0500

    feat: findUser method to make request to Github users endpoint

---
+++ app/api/index.js
@@ -0,0 +1,12 @@
+const URL = "https://api.github.com/";
+
+export const findUser = userName => {
+  const endpointURL = `${URL}/users/${userName}`;
+  const options = {
+    method: 'GET',
+    headers: {
+      'Accept': "application/vnd.github.v3+json"
+    }
+  };
+  return fetch(endpointURL, options);
+};
